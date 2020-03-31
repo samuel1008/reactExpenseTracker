481d52db9d0281cc4db786e0149bda3bbf604077
Sam <samuel1008@gmail.com>
2020-03-31 02:08:52 -0700

    First commit, added basic layout and first form component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, Container, Jumbotron } from 'reactstrap';
+import { useState } from 'react';
+import Logo from './logo.svg'
 
-function App() {
+//import component
+import Form from './components/Form';
+
+const ALL_EXPENSES = [
+  { id: 1, name: 'Buy a book', amount: 20 },
+  { id: 2, name: 'Buy a milk', amount: 5 },
+  { id: 3, name: 'Book a flight ticket', amount: 225 }
+]
+
+const App = () => {
+
+  //useState hook
+  const [ expense, setExpense ] = useState(ALL_EXPENSES)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="text-center">
+      <Jumbotron fluid>
+
+        <h3 className="display-6">
+          Expense Tracker App
+          <img src={Logo} style={{ width: 50, height: 50 }} alt="react-logo" />
+        </h3>
+
+        <div>
+          <p>
+            Total Expense: {' '}
+            <span className="text-success">
+              ${' '}
+              {expense.reduce((accumulator, currentValue) => {
+                return (accumulator += parseInt(currentValue.amount))
+              }, 0)}
+            </span>
+          </p>
+        </div>
+        <Form />      
+      </Jumbotron>
+    </Container>
   );
 }
 
